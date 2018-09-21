8a78c182d4d8b6556600927a1e8b9a910da9eddc
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-21 17:48:05 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -86,7 +86,7 @@ def adding_matz
 		  :languages => ["LISP", "C"]
 		 }
 		 programmer_hash
-			
+
 
 
 end
@@ -129,5 +129,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 programmer_hash
 
 end
